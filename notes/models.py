7593b040b92f3746fb3f8a754d4b5a4f4c1acac8
lucamelao <63018319+lucamelao@users.noreply.github.com>
2021-09-27 17:57:47 -0300

    saving tags

--- notes/models.py
+++ notes/models.py
@@ -2,11 +2,19 @@ from django.db import models
 
 # Create your models here.
 
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+    
+    def __str__(self):
+        # TAG
+        return f"{self.name}"
+
 # Nesse código cria-se um modelo chamado Note que possui um atributo title, que será 
 # mapeado no banco de dados em uma coluna cujos valores são strings de no máximo 200 caracteres.
 class Note(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
+    tag = models.ForeignKey(Tag, null = True, on_delete = models.SET_NULL)
 
     def __str__(self):
         # ID. TITULO
--- notes/views.py
+++ notes/views.py
@@ -1,32 +1,55 @@
 from django.shortcuts import render, redirect
-from .models import Note
+from .models import Note, Tag
 
 
 def index(request):
     if request.method == 'POST':
         title = request.POST.get('titulo')
         content = request.POST.get('detalhes')
+        tag = request.POST.get('tag')
+        # filtra dentre as tags, a tag com o nome
+        filtered_tags = Tag.objects.filter(name = tag)
+
+        # Cada anotação pode ter no máximo uma tag (pode não ter nenhuma)
+        
+        # para o caso de não ter nenhuma
+        if len(filtered_tags) == 0:
+            new_tag = Tag(name = tag)
+            new_tag.save()
+        else:
+            # caso já tenha
+            new_tag = filtered_tags[0]
+
         # Criando um novo Note (objeto) no banco de dados
-        new_note = Note(title = title, content = content)
+        new_note = Note(title = title, content = content, tag = new_tag)
         # Salva o objeto no db
         new_note.save()
         return redirect('index')
     else:
     # o modelo Note é importado e são carregadas todas as entradas dessa tabela 
     # Carregando os dados do banco de dados, tem todas as notas contidas nele
-        all_notes = Note.objects.all()
-        return render(request, 'notes/index.html', {'notes': all_notes})
+        notes = Note.objects.all()
+        tags = Tag.objects.all()
+        return render(request, 'notes/index.html', {'notes': notes, 'tags': tags})
 
 def remove_note(request):
     if request.method == 'POST':
         # pega o ID da note que será removida
         id = request.POST.get('id')
+        tag = request.POST.get('tag')
         # filtra pelo ID pego na linha anterior e deleta do db
         Note.objects.filter(id=id).delete()
+
+        if tag != '':
+            notes_with_tag = Note.objects.filter(tag = tag)
+            if len(notes_with_tag) == 0:
+                Tag.objects.filter(name = tag).delete()
+                
         return redirect('index')
     else:
-        all_notes = Note.objects.all()
-        return render(request, 'notes/index.html', {'notes': all_notes})
+        notes = Note.objects.all()
+        tags = Tag.objects.all()
+        return render(request, 'notes/index.html', {'notes': notes}, {'tags': tags})
     
 def update_note(request):
     if request.method == 'POST':
@@ -34,6 +57,7 @@ def update_note(request):
         id = request.POST.get('id')
         title = request.POST.get('title')
         content = request.POST.get('content')
+        # tag = request.POST.get('tag')
         # Atualiza e salva a note no db
         modified_note = Note(id = id, title = title, content = content)
         modified_note.save()
@@ -42,4 +66,17 @@ def update_note(request):
     # o modelo Note é importado e são carregadas todas as entradas dessa tabela 
     # Carregando os dados do banco de dados, tem todas as notas contidas nele
         all_notes = Note.objects.all()
+        # tags = Tag.objects.all()
         return render(request, 'notes/index.html', {'notes': all_notes})
+        # , {'tags': tags}
+
+def group_per_tag(request):
+    tag_name = request.POST.get('tag')
+    print(f'TAG NAME: {tag_name}')
+    group = Note.objects.filter(tag__name = tag_name)
+    print(f'GRUPO: {len(group)}')
+    return render(request, 'notes/tagNotes.html', {'notes': group, 'tag': tag_name})
+
+def list_tags(request):
+    list = Tag.objects.all()
+    return render(request, 'notes/tagsList.html', {'tags': list})
